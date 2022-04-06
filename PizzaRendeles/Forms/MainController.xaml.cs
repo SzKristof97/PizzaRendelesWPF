2f75420ae4ab7685d53129238c4fc0dd9c4aa91f
Szabó Kristóf <szkristof97.gyula@gmail.com>
2022-04-06 11:49:25 +0200

    Login And Main Form + Order and User Control

---
+++ PizzaRendeles/Forms/MainController.xaml.cs
@@ -0,0 +1,112 @@
+﻿using PizzaRendeles.UserControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PizzaRendeles.Forms
+{
+    /// <summary>
+    /// Interaction logic for MainController.xaml
+    /// </summary>
+    public partial class MainController : Window
+    {
+        private MainWindow Core;
+        private string ucContent;
+
+        public string UCContent {
+            get
+            {
+                return ucContent;
+            }
+            set
+            {
+                ucContent = value;
+                OnContentChanged(value);
+            }
+        }
+
+        public MainController(MainWindow core)
+        {
+            this.Core = core;
+            InitializeComponent();
+        }
+
+        #region exit button Events
+
+        private void exitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Core.Visibility = Visibility.Visible;
+            this.Close();
+        }
+
+        private void exitBtn_MouseEnter(object sender, MouseEventArgs e)
+        {
+            exitBtn.Foreground = new SolidColorBrush(Colors.Red);
+        }
+
+        private void exitBtn_MouseLeave(object sender, MouseEventArgs e)
+        {
+            exitBtn.Foreground = new SolidColorBrush(Colors.White);
+        }
+
+        #endregion
+
+        #region Mouse Enter / Leave
+
+        private void button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (e == null || e.Source == null) return;
+
+            Button button = (Button)e.Source;
+            if (button == null) return;
+
+            button.Foreground = new SolidColorBrush(Colors.Black);
+        }
+
+        private void button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (e == null || e.Source == null) return;
+
+            Button button = (Button)e.Source;
+            if (button == null) return;
+
+            button.Foreground = new SolidColorBrush(Colors.White);
+        }
+
+        #endregion
+
+        #region Content Change Events
+
+        private void OnContentChanged(string value)
+        {
+            switch (value)
+            {
+                case "orders": { contentManager.Content = new OrderUC(); break; }
+                case "users": { contentManager.Content = new UsersUC(); break; }
+                default: { break; }
+            }
+        }
+
+        #endregion
+
+        private void orderBtn_Click(object sender, RoutedEventArgs e)
+        {
+            UCContent = "orders";
+        }
+
+        private void usersBtn_Click(object sender, RoutedEventArgs e)
+        {
+            UCContent = "users";
+        }
+    }
+}
--- PizzaRendeles/MainWindow.xaml.cs
+++ PizzaRendeles/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PizzaRendeles.Forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,5 +25,79 @@ namespace PizzaRendeles
         {
             InitializeComponent();
         }
+
+        #region Exit button Events
+        private void exitBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            Environment.Exit(0);
+        }
+
+        private void exitBtn_MouseEnter(object sender, MouseEventArgs e)
+        {
+            exitBtn.Foreground = new SolidColorBrush(Colors.Red);
+        }
+
+        private void exitBtn_MouseLeave(object sender, MouseEventArgs e)
+        {
+            exitBtn.Foreground = new SolidColorBrush(Colors.White);
+        }
+        #endregion
+
+        #region Login button Events
+        private void loginBtn_MouseEnter(object sender, MouseEventArgs e)
+        {
+            loginBtn.Foreground = new SolidColorBrush(Colors.Black);
+        }
+
+        private void loginBtn_MouseLeave(object sender, MouseEventArgs e)
+        {
+            loginBtn.Foreground = new SolidColorBrush(Colors.White);
+        }
+
+        private void loginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string username = usernameInput.Text;
+            string password = userpasswordInput.Password;
+
+            if (String.IsNullOrEmpty(username) || username.Length < 4)
+            {
+                usernameInput.BorderBrush = new SolidColorBrush(Colors.Red);
+                MessageBox.Show("The username cannot be empty\nor less then 4 character!", "Username Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            usernameInput.BorderBrush = new SolidColorBrush(Colors.White);
+
+            if (String.IsNullOrEmpty(password) || password.Length < 8)
+            {
+                userpasswordInput.BorderBrush = new SolidColorBrush(Colors.Red);
+                MessageBox.Show("The password cannot be empty\nor less then 8 character!", "Username Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            userpasswordInput.BorderBrush = new SolidColorBrush(Colors.White);
+
+            // Database here
+            bool isValid = true;
+
+            // Validation is correct
+            if (isValid)
+            {
+                this.Visibility = Visibility.Hidden;
+
+                new MainController(this).Show();
+            }
+        }
+
+        #endregion
+
+        #region Drag Events
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.GetPosition(this).Y <= 60)
+                this.DragMove();
+        }
+
+        #endregion
     }
 }
